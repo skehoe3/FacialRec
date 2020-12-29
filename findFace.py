0e2b483528c5b50fa2199a7e298920a442ca84ca
Kehoe, Shannon <Shannon.Kehoe@dtms.de>
2020-12-29 17:54:05 +0100

    goes through all faces, but saves the same one each time

--- findFace.py
+++ findFace.py
@@ -12,12 +12,13 @@ import cv2
 import matplotlib.pyplot as plt
 import os
 from tqdm import tqdm
+import uuid
 # load the file
 
 #!Tiff images need to be converted to a different data type!
-FOLDER = "/Users/shannonkehoe/Desktop/Git/FacialRec/test_one"
+SOURCE_FOLDER = "/Users/shannonkehoe/Desktop/Git/FacialRec/test_one"
 FILES = [f for f in os.listdir(
-    FOLDER) if os.path.isfile(os.path.join(FOLDER, f)) and f != ".DS_Store"]  # filters out the MAC file
+    SOURCE_FOLDER) if os.path.isfile(os.path.join(SOURCE_FOLDER, f)) and f != ".DS_Store"]  # filters out the MAC file
 HAARCC = cv2.CascadeClassifier(
     'haarcascade_frontalface_alt2.xml')
 
@@ -26,39 +27,38 @@ def convertToRGB(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 
-face_locations = {}
-# ; throw this into a function, then throw the function inside of tqdm())
-for i in FILES:
-    #  Loading the image to be tested
-    # print(
-    #     FOLDER + '/' + i == "/Users/shannonkehoe/Desktop/Git/FacialRec/test_one/IMG_2078.JPG")
-    image = cv2.imread(
-        FOLDER + '/' + i)
-  #  print(i)
-    # Converting to grayscale as opencv expects detector takes in input gray scale images
-    test_image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # print(test_image_gray)
-    # ; orig
-    faces_rects = HAARCC.detectMultiScale(
-        test_image_gray)
-    face_locations[i] = faces_rects
-    print(len(faces_rects))
-   # print(faces_rects)
-    # #! only for testing!
-    # # subset the faces out and save as seperate images.
-    # print(faces_rects[1])
-    # print(faces_rects[0][0])
+def crop(narray, image):
+    """
+    Takes a single numpy array of x,y,w,h and selects only that area from the image. 
+    Args:
+        narray (numpy array): a numpy array of x-coord,y-coord,width,height 
+        image ([type]): numpy array representation of an image
+
+    Returns:
+        numpy array: subset of the input image
+    """
     xstart = faces_rects[0][0]
     xstop = xstart + faces_rects[0][2]
     ystart = faces_rects[0][1]
     ystop = ystart + faces_rects[0][3]
+    return image[xstart:xstop, ystart:ystop]
 
-    # ; faces_rects --> (x, y, w, h)
-    cropped = test_image_gray[xstart:xstop, ystart:ystop]
-    cv2.imwrite('face.png',
-                convertToRGB(cropped))
-# ; so this is my "dataset" containing the locations of all faces in the folder.
-# print(face_locations)
 
-# ; for efficiency, make a subfolder called "faces" and store the faces we found, and ONLY the faces we found, in that location.
-# ; this will make it faster to compare the faces later.
+face_locations = {}
+# ; throw this into a function, then throw the function inside of tqdm())
+for i in FILES:
+    image = cv2.imread(SOURCE_FOLDER + '/' + i)
+    test_image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces_rects = HAARCC.detectMultiScale(
+        test_image_gray)
+    imname = i.split(".")[0]
+    # ; loopd crops
+    for row in range(0, len(faces_rects)):
+        print(row)
+        cropped = crop(faces_rects[row], image)
+        # save faces to a folder using the original image name and a unique id seperated by a '|'
+        # pipe was used as some photos have a uuid style id already ( lots of dashes ) and wanted to be able to
+        # differentiate between an images given name and its id as assigned through this program
+        guid = str(uuid.uuid4())
+        cv2.imwrite('faces/' + imname+'|' +
+                    guid + '.png', cropped)
